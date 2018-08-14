ee8ab913239c816ec22ac91910f7b2f1c52f4bcb
huangbo <534551900@qq.com>
2018-08-14 21:11:54 +0800

    所有地区的最低温度爬取完成，接下来还需要做简单的可视化处理

--- weather_spider.py
+++ weather_spider.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import html5lib
 
 def parse_page(url):
     headers = {
@@ -9,7 +9,7 @@ def parse_page(url):
     }
     response = requests.get(url,headers = headers)
     text = response.content.decode('utf8')
-    soup = BeautifulSoup(text,'lxml')
+    soup = BeautifulSoup(text,'html5lib')
     # print(response.content.decode('utf-8'))
     conMidtable = soup.find('div',class_='conMidtab')
     tables = conMidtable.find_all('table')
@@ -17,21 +17,30 @@ def parse_page(url):
         trs = table.find_all('tr')[2:]
         for index,tr in enumerate(trs):
             tds = tr.find_all('td')
+            city_td = tds[0]
             if index == 0:
                 city_td = tds[1]
-            else:
-                city_td = tds[0]
             city = list(city_td.stripped_strings)[0]
             temp_td = tds[-2]
-            min_temp = list(temp_td.stripped_strings)
-            print(min_temp)
+            min_temp = list(temp_td.stripped_strings)[0]
+            # print(min_temp)
             print({'city':city,'min_temp':min_temp})
 
 
 def main():
-    # url = "http://www.weather.com.cn/textFC/hb.shtml"
-    url = "http://www.weather.com.cn/textFC/db.shtml"
-    parse_page(url)
+    url_list = [
+        # 'http://www.weather.com.cn/textFC/hb.shtml', # 华北地区
+        # 'http://www.weather.com.cn/textFC/db.shtml', # 东北地区
+        # 'http://www.weather.com.cn/textFC/hd.shtml', # 华东地区
+        # 'http://www.weather.com.cn/textFC/hz.shtml', # 华中地区
+        # 'http://www.weather.com.cn/textFC/hn.shtml', # 华南地区
+        # 'http://www.weather.com.cn/textFC/xb.shtml', # 西北地区
+        # 'http://www.weather.com.cn/textFC/xn.shtml', # 西南地区
+        'http://www.weather.com.cn/textFC/gat.shtml', # 港澳台地区
+    ]
+    for url in url_list:
+        parse_page(url)
+        break
 
 
 if __name__ == '__main__':
